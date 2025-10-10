b8c5c7819ff24887b872698ad48b344357203e13
luispedraza478 <20243ds159@utez.edu.mx>
2025-10-10 15:08:52 -0600

    Add files via upload

--- App.java
+++ App.java
@@ -1,32 +1,27 @@
-import java.util.Queue;
-import java.util.ArrayDeque;
-
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        int[][] arrTest = {
+            {1},
+            {6, 3, 1},
+            {1, 2, 3, 4},
+            {0, 5, 3, 0},
+            {2, 5, 9},
+            {2, 4, 8, 9, 10}
+        };
 
-        Queue<Cliente> cola = new ArrayDeque<>();
+        int[][] arrBiE = new int[6][];
+        arrBiE[0] = new int[]{1, 2};
+        arrBiE[1] = new int[]{6, 3, 1, 2};
+        arrBiE[2] = new int[]{1, 2, 3, 4, 5};
+        arrBiE[3] = new int[]{0, 5, 3, 0, 1};
+        arrBiE[4] = new int[]{2, 5, 9, 10};
+        arrBiE[5] = new int[]{2, 4, 8, 9, 10, 11};
 
-        Cliente c1 = new Cliente(1, "Carlos");
-        c1.agregarDocumento("Doc1");
-        c1.agregarDocumento("Doc2");
-
-        Cliente c2 = new Cliente(2, "María");
-        c2.agregarDocumento("Doc1");
-        c2.agregarDocumento("Doc2");
-
-        Cliente c3 = new Cliente(3, "José");
-        c3.agregarDocumento("Doc1");
-        c3.agregarDocumento("Doc2");
-
-        cola.add(c1);
-        cola.add(c2);
-        cola.add(c3);
-
-        Cliente atendiendo = cola.poll(); 
-        if (atendiendo != null) {
-            atendiendo.atender();
+        for (int i = 0; i < arrBiE.length; i++) {
+            for (int j = 0; j < arrBiE[i].length; j++) {
+                System.out.print(arrBiE[i][j] + " ");
+            }
+            System.out.println();
         }
-
-        System.out.println("Cola restante: " + cola);
     }
 }
